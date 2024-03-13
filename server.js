090c8ca8307f7fd2d83c72d785b0d06e14d26a6b
mihirjariwala1311 <81627330+mihir1314@users.noreply.github.com>
2024-03-13 15:08:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = 3306;
 app.use("/uploads", express.static("Uploads"));
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:5173"],
+  origin:["http://mihirjariwala.rf.gd"],
   methods:["POST","GET"],
   credentials:true
 } ));
